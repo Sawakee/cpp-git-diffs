2cb74373c188535c674d5a5bab2cebb6d2a27492
Sawakee <sawake.inc@gmail.com>
2025-08-17 16:00:18 +0900

    add example

---
+++ example/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// This is a simple C++ program that prints "Hello, World!" to the console.
+int main() {
+  // Print a greeting message
+  std::cout << "Hello, World!" << std::endl;
+  // Return 0 to indicate successful execution
+  return 0;
+}
